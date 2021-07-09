5d9dc1f1602b4d163f8cda3d61e4158742738d6e
VladRynchenko <41292054+VladRynchenko@users.noreply.github.com>
2021-07-09 14:54:30 +0300

    Add task for Recursion

---
+++ src/Recursion.java
@@ -0,0 +1,30 @@
+import java.io.Serial;
+
+public class Recursion {
+    public static void main(String[] args) {
+        System.out.println(power(2, 3));
+        makeBranch(0, 10);
+    }
+
+    static int power(int number, int power) {
+        if (power != 0) {
+            return (number * power(number, power - 1));
+        }
+        return number;
+    }
+
+    static void makeBranch(int left, int right) {
+        int centr = (left + right) / 2;
+        for (int i = left; i < right; i++) {
+            if (i == centr) {
+                System.out.print("X");
+            } else {
+                System.out.print("-");
+            }
+        }
+        if (left != right) {
+            makeBranch(left, centr);
+            makeBranch(centr, right - 1);
+        }
+    }
+}
